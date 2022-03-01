5420d3277f21efdbb028b4cf85b91fa710fcbec9
Hashiraee <hasanisraeli@hotmail.com>
2022-03-01 18:10:45 +0100

    Rust analyzer changes

--- nvim/.config/nvim/lua/lsp/settings/rust_analyzer.lua
+++ nvim/.config/nvim/lua/lsp/settings/rust_analyzer.lua
@@ -14,10 +14,6 @@ return {
             procMacro = {
                 enable = true,
             },
-
-            checkOnSave = {
-                command = "clippy",
-            },
         },
     },
 }
