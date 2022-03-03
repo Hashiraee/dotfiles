210975f1a7ed6114e8281ba56cf5696b8d35bad0
Hashiraee <hasanisraeli@hotmail.com>
2022-03-03 18:43:55 +0100

    lsp keybind changes

--- nvim/.config/nvim/lua/lsp/handlers.lua
+++ nvim/.config/nvim/lua/lsp/handlers.lua
@@ -72,10 +72,21 @@ local function lsp_keymaps(bufnr)
 
     -- Workspace stuff
     vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-
     vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(
+        bufnr,
+        'n',
+        '<space>wr',
+        '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>',
+        opts
+    )
+    vim.api.nvim_buf_set_keymap(
+        bufnr,
+        'n',
+        '<space>wl',
+        '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>',
+        opts
+    )
 
     -- Code actions
     vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
