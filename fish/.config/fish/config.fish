a33be1259748bd527d11ca9fef2bd77c8dfeead3
Hashiraee <hasanisraeli@hotmail.com>
2022-05-10 20:53:55 +0200

    Some cleanup

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,6 +2,10 @@ abbr -a n nvim
 abbr -a v vim
 abbr -a cnvim cd ~/.dotfiles/nvim/.config/nvim
 
+alias ls="exa"
+alias ll="exa -l"
+alias la="exa -al"
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
