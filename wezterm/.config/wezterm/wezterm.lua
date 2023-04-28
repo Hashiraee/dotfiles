51619c64d31e3553b5fa29f339c97eb5d5ae4d96
Hashiraee <hasanisraeli@hotmail.com>
2023-04-28 16:43:39 +0200

    Update dots

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,26 @@
+return {
+    -- Colorscheme
+    color_scheme = "Catppuccin Mocha",
+
+    -- Font size
+    font_size = 16,
+
+    -- Tab bar stye
+    tab_bar_at_bottom = true,
+    hide_tab_bar_if_only_one_tab = true,
+    use_fancy_tab_bar = true,
+
+    -- Window decorations
+    window_decorations = "RESIZE",
+
+    -- No confirmation on quit
+    window_close_confirmation = "NeverPrompt",
+
+    -- Padding
+    window_padding = {
+        left = 0,
+        right = 0,
+        top = 0,
+        bottom = 0,
+    },
+}
