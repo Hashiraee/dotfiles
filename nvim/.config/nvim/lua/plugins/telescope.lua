e48d33c6e78442f34ec5632235d0a50f4f74255a
Hashiraee <hasanisraeli@hotmail.com>
2025-03-15 14:57:00 +0100

    update: add extra keybind

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -10,7 +10,7 @@ function Plugin.config()
     require("telescope").setup({
         defaults = {
             file_ignore_patterns = {
-                ".git/"
+                ".git/", ".venv/", "venv/", "node_modules/"
             }
         },
 
@@ -57,13 +57,14 @@ function Plugin.config()
 
     -- Telescope keymaps
     local builtin = require("telescope.builtin")
+    vim.keymap.set("n", "<Leader>fi", function() builtin.find_files({ hidden = true, no_ignore = true }) end)
     vim.keymap.set("n", "<Leader>ff", function() builtin.find_files({ hidden = true }) end)
     vim.keymap.set("n", "<Leader>fn", function() builtin.find_files({ cwd = vim.fn.stdpath("config") }) end)
     vim.keymap.set("n", "<Leader>fb", function() builtin.buffers() end)
     vim.keymap.set("n", "<Leader>fc", function() builtin.current_buffer_fuzzy_find() end)
     vim.keymap.set("n", "<Leader>fh", function() builtin.help_tags() end)
     vim.keymap.set("n", "<Leader>fg", function() builtin.live_grep() end)
-    vim.keymap.set("n", "<Leader>fs", function() builtin.grep_string({ search = vim.fn.input("Grep > ")}) end)
+    vim.keymap.set("n", "<Leader>fs", function() builtin.grep_string({ search = vim.fn.input("Grep > ") }) end)
 end
 
 return Plugin
