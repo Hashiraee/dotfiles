38ef9a04920bee54d937bcee31a8ca6644bf9832
Hashiraee <hasanisraeli@hotmail.com>
2025-09-07 19:33:05 +0200

    update: select own keybindings

--- nvim/.config/nvim/lua/plugins/blink.lua
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -6,9 +6,23 @@ Plugin.version = "1.*"
 
 Plugin.opts = {
     keymap = {
-        preset = "enter",
-        ['<C-d>'] = { 'scroll_documentation_down' , 'fallback' },
-        ['<C-u>'] = { 'scroll_documentation_up' , 'fallback' },
+        preset = "none",
+        ['<C-\\>'] = { 'show', 'show_documentation', 'hide_documentation' },
+        ['<C-e>'] = { 'hide', 'fallback' },
+        ['<CR>'] = { 'accept', 'fallback' },
+
+        ['<Tab>'] = { 'select_next', 'snippet_forward', 'fallback' },
+        ['<S-Tab>'] = { 'select_prev',  'snippet_backward', 'fallback' },
+
+        ['<C-n>'] = { 'select_next', 'snippet_forward', 'fallback' },
+        ['<C-p>'] = { 'select_prev', 'snippet_backward', 'fallback' },
+
+        ['<C-u>'] = { 'scroll_documentation_up', 'fallback' },
+        ['<C-d>'] = { 'scroll_documentation_down', 'fallback' },
+        ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
+        ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
+
+        ['<C-k>'] = { 'show_signature', 'hide_signature', 'fallback' },
     },
 
     completion = {
