d7a97695e2a60ff38b151da8d62469ba53a8df58
Hashiraee <hasanisraeli@hotmail.com>
2026-01-03 16:12:20 +0100

    misc: disable colorscheme

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -17,20 +17,20 @@ local Themes = {
             vim.cmd.colorscheme("rose-pine")
         end
     },
-    {
-        "catppuccin/nvim",
-        name = "catppuccin",
-        priority = 1000,
-        config = function()
-            local catppuccin = require("catppuccin")
-            catppuccin.setup({
-                flavour = "mocha",
-                transparent_background = false,
-            })
-
-            -- vim.cmd.colorscheme("catppuccin")
-        end
-    },
+    -- {
+    --     "catppuccin/nvim",
+    --     name = "catppuccin",
+    --     priority = 1000,
+    --     config = function()
+    --         local catppuccin = require("catppuccin")
+    --         catppuccin.setup({
+    --             flavour = "mocha",
+    --             transparent_background = false,
+    --         })
+    --
+    --         vim.cmd.colorscheme("catppuccin")
+    --     end
+    -- },
     {
         "EdenEast/nightfox.nvim",
         name = "nightfox",
