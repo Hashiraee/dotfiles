8fe8408e9b20ec563a1de2a481ebb8d491aa7dee
Hashiraee <hasanisraeli@hotmail.com>
2026-01-16 01:33:47 +0100

    feat: gopls lsp configuration for nvim

--- nvim/.config/nvim/lsp/gopls.lua
+++ nvim/.config/nvim/lsp/gopls.lua
@@ -1,15 +1,24 @@
--- Install with go
+-- Install with: go install golang.org/x/tools/gopls@latest
 
 ---@type vim.lsp.Config
 return {
     cmd = { 'gopls' },
-    filetypes = { 'go', 'gomod' },
     root_markers = {'go.mod', '.git' },
+    filetypes = { 'go', 'gomod' },
     settings = {
         gopls = {
             analyses = {
                 unusedparams = true,
                 shadow = true,
+            },
+            hints = {
+                assignVariableTypes = true,
+                compositeLiteralFields = true,
+                compositeLiteralTypes = true,
+                constantValues = true,
+                functionTypeParameters = true,
+                parameterNames = true,
+                rangeVariableTypes = true,
             },
         },
     },
