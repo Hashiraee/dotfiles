ed5b148b5e98dc9d6bf59e36f6891d607827f9a2
Hashiraee <hasanisraeli@hotmail.com>
2026-04-26 16:24:47 +0200

    obsidian.nvim: add keymaps

--- nvim/.config/nvim/lua/plugins/obsidian.lua
+++ nvim/.config/nvim/lua/plugins/obsidian.lua
@@ -15,11 +15,22 @@ Plugin.opts = {
             path = "~/Workspace/github.com/Hashiraee/notes",
         },
     },
+    daily_notes = {
+        folder = "daily",
+    },
 }
 
 function Plugin.config()
     local obsidian = require("obsidian")
     obsidian.setup(Plugin.opts)
+
+    -- Keymaps
+    vim.keymap.set("n", "<leader>of", "<cmd>Obsidian quick_switch<cr>")
+    vim.keymap.set("n", "<leader>os", "<cmd>Obsidian search<cr>")
+    vim.keymap.set("n", "<leader>od", "<cmd>Obsidian dailies<cr>")
+    vim.keymap.set("n", "<leader>ot", "<cmd>Obsidian today<cr>")
+    vim.keymap.set("n", "<leader>on", "<cmd>Obsidian tomorrow<cr>")
+    vim.keymap.set("n", "<leader>op", "<cmd>Obsidian yesterday<cr>")
 end
 
 return Plugin
